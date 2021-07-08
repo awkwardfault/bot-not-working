7f43e1b9cdcdbccdaafbb6876a058086c3bcdbeb
Matthew Blau <mrb1105@gmail.com>
2021-07-07 23:00:42 -0400

    various async related changes

--- cogs/moviedb.py
+++ cogs/moviedb.py
@@ -1,22 +1,35 @@
-import pprint
-import requests
+import pprint as pp
+from discord.ext import commands
+import aiohttp
+from discord.ext.commands.core import command
 
 
-search_query = 'endgame'
-api_key = 'api_key'
-api_base_url = "https://api.themoviedb.org/3"
-endpoint_path = f"/search/movie"
-endpoint = f"{api_base_url}{endpoint_path}?api_key={api_key}&query={search_query}&page=1&include_adult=true"
+class moviedb(commands.Cog):
+    def __init__(self, bot) -> None:
+        self.bot = bot
+
+    @commands.command(name="movie")
+    async def movie(self, context, search_query):
+        api_key = 'api_key'
+        url = "https://api.themoviedb.org/3/search/movie"
+
+        params = {
+            'api_key': f"{api_key}",
+            'query': f"{search_query}",
+            'page': '1',
+            "include_adult": "true"
+        }
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, params=params) as response:
+                if response.status == 200:
+                    data = await response.json()
+                    results = data['results']
+                    movie_info = results[0]
+                    for movie in movie_info:
+                        _description = movie['overview']
+                        print(_description)
+                        await context.send(_description)
 
 
-r= requests.get(endpoint)
-data = r.json()
-results = data['results']
-results1 = results[0]
-for result in results1:
-    _description= result['overview']
-   
-
-
-    print(_description)
-    
+def setup(bot):
+    bot.add_cog(moviedb(bot))
